ad8967f56cb8b3cd3fba1c24ef032019ca9a7023
Tejas Sawant <tejassawant@Tejass-MacBook-Air.local>
2019-08-06 00:02:50 +0530

    tejassourcefile

---
+++ myapp/src/app/CODE/Routing(Tejas)/app.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from "@angular/router";
+import { ProductComponent } from "../Routing(Tejas)/product/product.component";
+
+export const routes: Route[] = [
+  {
+    path: "product",
+    component: ProductComponent
+  }
+];
